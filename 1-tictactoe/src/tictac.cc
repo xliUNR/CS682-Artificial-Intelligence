b19161387713bb209358456ff39823bce6c1198f
David Feil-Seifer <dave@cse.unr.edu>
2018-08-18 00:35:51 -0700

    initial HelloWorld build of project 1

---
+++ 1-tictactoe/src/tictac.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main( int argc, char* argv[] )
+{
+
+	// load in current board state
+
+	// make move
+
+	// save board state
+
+	printf( "Hello World!\n");
+
+	return 0;
+}
