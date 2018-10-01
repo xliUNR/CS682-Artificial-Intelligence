d704169ba9c11a3ef3b2f6fc1b7c847b1cfb7795
xliUNR <xli@nevada.unr.edu>
2018-09-30 17:15:36 -0700

    added build files

---
+++ 1-tictactoe/build/CMakeFiles/tictactoe.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xl/git-repos/cs482-fall2018/1-tictactoe/src/tictac.cc" "/home/xl/git-repos/cs482-fall2018/1-tictactoe/build/CMakeFiles/tictactoe.dir/src/tictac.cc.o"
+  "/home/xl/git-repos/cs482-fall2018/1-tictactoe/src/tictac_support.cc" "/home/xl/git-repos/cs482-fall2018/1-tictactoe/build/CMakeFiles/tictactoe.dir/src/tictac_support.cc.o"
+  "/home/xl/git-repos/cs482-fall2018/1-tictactoe/src/tictac_turnin.cc" "/home/xl/git-repos/cs482-fall2018/1-tictactoe/build/CMakeFiles/tictactoe.dir/src/tictac_turnin.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
