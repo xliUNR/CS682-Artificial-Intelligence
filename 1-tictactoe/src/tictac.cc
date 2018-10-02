687962aa07e21b2db2a44f365675d6229137185c
xliUNR <xli@nevada.unr.edu>
2018-10-02 02:17:50 -0700

    now plays full game

--- 1-tictactoe/src/tictac.cc
+++ 1-tictactoe/src/tictac.cc
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <iostream>
 #include <tictac_support.h>
 
 int main( int argc, char* argv[] )
 {
 
 	int board[3][3], move[3][3];
-
+   int gameState = 0;
 	// load in current board state
 	load_board( argv[1], board );
 
 	// make move
-	make_move( board );
+   while( gameState == 0 ){
+	  gameState =  make_move( board );
+
+   }
+
 	
 	// save board state
 	save_board( argv[2], board );
