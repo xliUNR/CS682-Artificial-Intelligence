bf877b22a5d034588e7560e45b9ad092a5980f19
xliUNR <xli@nevada.unr.edu>
2018-10-28 19:33:47 -0700

    added files for project 2 - Niave Bayes spam/ham

---
+++ 2-spamfilter/classify.cc
@@ -0,0 +1,3 @@
+//This file contains the classifier. Input is .csv file of task, output will give each item a specifier of ham or spam.
+//Written by Xiang Li for CS682: Artificial Intelligence
+//20181028
---
+++ 2-spamfilter/training.cc
@@ -0,0 +1,4 @@
+//This file contains training for classifier
+//Written by Xiang Li for CS682: Artificial Intelligence
+//20181028
+//
